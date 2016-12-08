0fd73395fcd0e02e599edaf42e3bd1555d5799a6
mamta1010 <smstuki124@gmail.com>
2016-12-08 17:17:10 +0530

    server file

---
+++ app.js
@@ -0,0 +1,46 @@
+ var express=require("express");
+ var bodyp=require("body-parser");
+ var http=require("http");
+ var mor=require("morgan");
+ var path = require("path");
+ var app=express();
+ var jwt = require('express-jwt');
+var rsaValidation = require('auth0-api-jwt-rsa-validation');
+
+  app.use(mor('dev'));
+ app.use(bodyp.json());
+
+ app.use(bodyp.urlencoded({ extended: false }));
+ var jwtCheck = jwt({
+  secret: rsaValidation()
+});
+app.use(jwtCheck);
+
+
+
+  app.get('/',function(req,res){
+ 	res.sendFile(path.join(__dirname + '/index.html'));
+ });
+
+   app.post('/login' , function(req, res){
+ 	var username = req.body.uName;
+ 	var password =	req.body.pass;
+
+ 	console.log("Username was "+username+" Password was "+password);
+ 	res.send(username+" : "+password);
+ });
+
+app.get('*' ,function(req,res){
+ 	res.status(404).send('404 Error Page');
+ });
+
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({message:'Missing or invalid token'});
+  }
+});
+
+var server = http.createServer(app);
+server.listen(4884,function(){
+	console.log("lloading")
+});
